744a7d103bba96325dc6c4efda6ab0b68c2528f0
DevikaDinesh2102 <97044355+DevikaDinesh2102@users.noreply.github.com>
2022-01-21 11:49:53 +0530

    Add files via upload

---
+++ Assignment 1.1.py
@@ -0,0 +1,96 @@
+#ASSINGMENT 1.1
+
+#1 comparison and assignment operators
+# comparison operators
+a=8
+b=5
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+c=1
+d=-1
+print(c==d)
+print(c!=d)
+print(c>d)
+print(c<d)
+print(c>=d)
+print(c<=d)
+
+q=12
+p=12
+print(q==p)
+print(q!=p)
+print(q>p)
+print(q<p)
+print(q>=p)
+print(q<=p)
+
+x=5
+y=1
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+#assignment operators
+g=2
+g+=3
+print(g)
+g-=1
+print(g)
+g/=3
+print(g)
+g=4
+g*=3
+print(g)
+x = 5
+x%=3
+print(x)
+x = 99
+x//=3
+print(x)
+x = 7
+x **= 3
+print(x)
+x = 0
+x &= 6
+print(x)
+x = 3
+x &= 6
+print(x)
+x = 2
+x |= 9
+print(x)
+x = 12
+x ^= 8
+print(x)
+x = 1
+x >>=9
+print(x)
+x = 90
+x <<= 10
+print(x)
+
+
+# 2 if and else statement
+#a
+p=('it snows')
+a=input('condition:')
+if a==p:
+   print('i will go out')
+else:
+        print('i will not')
+    
+#b
+p=('it rains frogs')
+a=input('condition:')
+if a==p:
+   print('i will dance')
+else:
+        print('i will not dance')
