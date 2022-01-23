9360491160f70545d4e67def42b968f2d22e0ea5
DevikaDinesh2102 <97044355+DevikaDinesh2102@users.noreply.github.com>
2022-01-23 23:40:55 +0530

    Add files via upload

---
+++ Assingment1.py
@@ -0,0 +1,51 @@
+#1
+#float
+#float is included in the NUMERIC TYPE.It includes the integer part as well as the fractional part.
+
+#2-Use terminal to open apps
+#dir
+#cd C:\Users\HP\AppData\Roaming\Telegram Desktop
+
+#3-Practise all the arithmetic opt 
+
+x=5
+y=9
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+x1=1
+y1=5
+print(x1+y1)
+print(x1-y1)
+print(x1/y1)
+print(x1*y1)
+print(x1%y1)
+print(x1**y1)
+
+x2=20
+y2=10
+print(x+y2)
+print(x2-y2)
+print(x2*y2)
+print(x2/y2)
+print(x2%y2)
+print(x2**y2)
+
+x3=12
+y3=99
+print(x3+y3)
+print(x3-y3)
+print(x3*y3)
+print(x3/y3)
+print(x3%y3)
+print(x3**y3)
+
+#4-area of a circle
+radius=int(input("radius of the circle:"))
+pi=3.14
+Area=pi*radius**2
+print("area of the circle:",Area)
